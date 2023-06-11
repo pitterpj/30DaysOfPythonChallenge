a2dd45855be5d8ebe46b6947b4f9599392b6a2eb
Pitter <pedrojc@outlook.es>
2023-06-11 16:25:47 +0200

    Day01 #30DaysOfPythonChallenge

---
+++ 00_hello.py
@@ -0,0 +1,23 @@
+# Introducción
+# Día 1 - 30DaysOfPythonChallenge
+
+# Este es mi primer comentario en python
+# Hola mundo en Python
+print("Hello Python")
+
+'''
+Este es un 
+comentrario 
+en varias lineas
+'''
+
+# Tipos de datos:
+print('Hola') #str
+print(777) #entero
+print(7.54) #float
+print(True) #bool
+
+#Consultar el tipo de dato:
+print(type(True)) #bool
+print(type('Dato str')) #str
+
---
+++ 01_Day01.py
@@ -0,0 +1,21 @@
+# Open the python interactive shell and do the following operations. The operands are 3 and 4.
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Check the data types of the following data:
+print(type(17)) #integer
+print(type(6.8)) #float
+print(type(3.14)) #float
+print(type(7-3j)) #complex
+print(type(['Egipto','Python','Chanchito Feliz'])) #list
+print(type('Pitter')) #str
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+from math import sqrt
+print(sqrt((3-2)**2 + (8-10)**2))
