b0b04da3c56655d8d71c148bf6725dce4b9d77ee
Pitter <pedrojc@outlook.es>
2023-06-24 12:50:56 +0200

    Día 14 - 30DaysOfPythonChallenge

--- test.py
+++ test.py
@@ -1,20 +1,14 @@
-### CLASES ###
+#### Excepction Handling ###
+# Exercises - Day 14
+# Día 14 - 30DaysOfPythonChallenge
 
-<<<<<<< HEAD
-print((lambda a, b: a + b)(2,3))
-=======
-class Person: 
-    def __init__(self, name, surname):
-        self.full_name = f"{name} {surname}"
-    
-    def walk (self):
-        print(f"{self.full_name} está caminando")
-
-
-
-
-my_person = Person('Julian', 'Rodriguez')
-print(my_person.full_name)
-
-my_person.walk()
->>>>>>> aa7177b914842070222a66c0becc74ac89957a2f
+# 💻Exercises: Day 14
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+# . Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+fin,sw,nor,den,ice,*rest=names
+nordic_countrie = fin,sw,nor,den,ice
+es,ru=rest
+print(nordic_countrie)
+print(es)
+print(ru)
+# 🎉 CONGRATULATIONS ! 🎉
